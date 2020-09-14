91bc755e448e3ad2911f7b97c16a48c9e30532e6
Amil-Gaoul <amildream@gmail.com>
2020-09-14 18:33:30 +0700

    fix(action): fix action component

--- src/packages/core/src/action/__tests__/action.spec.ts
+++ src/packages/core/src/action/__tests__/action.spec.ts
@@ -1,21 +1,33 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {waitForAsync, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Component, DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
+import {EAction} from '../action';
 
-import { EAction } from '../action';
+@Component({
+    template: `
+        <button eAction>
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4.41431 3H19.5859C20.1382 3 20.5859 3.44772 20.5859 4C20.5859 4.26522 20.4805 4.51957 20.293 4.70711L14.0001 11V21L10.0001 18V11L3.7072 4.70711C3.31668 4.31658 3.31668 3.68342 3.7072 3.29289C3.89474 3.10536 4.14909 3 4.41431 3Z" />
+            </svg>
+            Filter Items Button
+        </button>
+    `
+})
+class TestAction { }
 
 describe('EAction', (): void => {
-    let component: EAction;
-    let fixture: ComponentFixture<EAction>;
+    let component: TestAction;
+    let fixture: ComponentFixture<TestAction>;
 
-    beforeEach(async((): void => {
+    beforeEach(waitForAsync((): void => {
         TestBed.configureTestingModule({
-            declarations: [ EAction ]
+            declarations: [ EAction, TestAction ]
         })
             .compileComponents();
     }));
 
     beforeEach((): void => {
-        fixture = TestBed.createComponent(EAction);
+        fixture = TestBed.createComponent(TestAction);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
@@ -24,12 +36,13 @@ describe('EAction', (): void => {
         expect(component).toBeTruthy();
     });
 
-    it('should click', (): void => {
-        spyOn(component.onClick, 'emit');
-        const action: HTMLElement = fixture.debugElement.query(By.css('._e_action')).nativeElement;
-        action.dispatchEvent(new MouseEvent('click'));
+    it('should have class "_e_action"', (): void => {
+        const action: DebugElement = fixture.debugElement.query(By.css('._e_action'));
+        expect(action).toBeTruthy();
+    });
 
-        fixture.detectChanges();
-        expect(component.onClick.emit).toHaveBeenCalled();
+    it('should have class "_e_action__icon"', (): void => {
+        const icon: DebugElement = fixture.debugElement.query(By.css('._e_action__icon'));
+        expect(icon).toBeTruthy();
     });
 });
---
+++ src/packages/core/src/action/demo/icon.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EIcon} from './icon';
+
+@NgModule({
+    declarations: [EIcon],
+    imports: [
+        CommonModule
+    ],
+    exports: [EIcon]
+})
+export class EIconModule { }
